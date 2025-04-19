98ddeae79a3ec4fb1a22ad820a72ead08e21514d
Felix Malvin <felix.malvin@students.mikroskil.ac.id>
2025-04-19 17:22:07 +0700

    Menambah file color.dart

---
+++ lib/commons/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static Color get primary => const Color(0xff0066FF);
+  static Color get secondary => const Color(0xffFFC524);
+  static Color get primaryText => const Color(0xff2E2E2E);
+  static Color get secondaryText => const Color(0xff585555);
+  static Color get white => const Color(0xffF9F9F9);
+}
