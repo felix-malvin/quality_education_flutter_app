47eb1f2234c6b39d973e99ef4cb3693323273836
Johannes Hartanto <johannes.hartanto@students.mikroskil.ac.id>
2025-04-20 14:22:09 +0700

    Menambah File Baru onboarding.dart

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Onboarding Page',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
