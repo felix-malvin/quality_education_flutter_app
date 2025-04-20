9b630f1fa889a3988f37902379e9009098d77b30
Erik Hamli <erik.hamli@students.mikroskil.ac.id>
2025-04-20 15:28:40 +0700

    Memodifikasi file onboarding.dart

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,16 +1,207 @@
 import 'package:flutter/material.dart';
+import 'package:quality_education_app/pages/signup.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
 
   @override
+  _OnboardingPage createState() => _OnboardingPage();
+}
+
+class _OnboardingPage extends State<OnboardingPage> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        _currentPage = _pageController.page!.round();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Onboarding Page',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                child: PageView(
+                  controller: _pageController,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/onboarding_1.png', height: 290),
+                          const SizedBox(height: 25),
+                          Text.rich(
+                            TextSpan(
+                              text: "Discover Your ",
+                              style: const TextStyle(
+                                fontSize: 26,
+                                color: Color(0xFF2E2E2E),
+                                fontWeight: FontWeight.w800,
+                              ),
+                              children: [
+                                const TextSpan(
+                                  text: "Learning",
+                                  style: TextStyle(color: Color(0xFFFFC524)),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Text(
+                            "Adventure",
+                            style: TextStyle(
+                              fontSize: 26,
+                              color: Color(0xFFFFC524),
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const SizedBox(
+                            width: 330,
+                            child: Text(
+                              "Start your journey to quality education today and unlock your full potential.",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xFF585555),
+                                fontWeight: FontWeight.w500,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          const SizedBox(height: 70),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 72, left: 30, right: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    if (_currentPage > 0)
+                      TextButton(
+                        onPressed: () {
+                          _pageController.previousPage(
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.easeInOut,
+                          );
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 1.5,
+                              color: const Color(0xFF0066FF),
+                            ),
+                          ),
+                          child: const Icon(
+                            Icons.arrow_back,
+                            color: Color(0xFF0066FF),
+                          ),
+                        ),
+                      )
+                    else
+                      const SizedBox(width: 45),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(3, (index) {
+                        return AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          margin: const EdgeInsets.symmetric(horizontal: 5),
+                          width: _currentPage == index ? 12 : 8,
+                          height: _currentPage == index ? 12 : 8,
+                          decoration: BoxDecoration(
+                            color:
+                                _currentPage == index
+                                    ? const Color(0xFFFFC524)
+                                    : Colors.grey,
+                            shape: BoxShape.circle,
+                          ),
+                        );
+                      }),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        if (_currentPage < 2) {
+                          _pageController.nextPage(
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.easeInOut,
+                          );
+                        } else {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SignupPage(),
+                            ),
+                          );
+                        }
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xFF0066FF),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 50,
+            right: 30,
+            child: TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignupPage()),
+                );
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.transparent,
+                ),
+                child: const Text(
+                  'Skip',
+                  style: TextStyle(
+                    color: Color(0xFF0066FF),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
