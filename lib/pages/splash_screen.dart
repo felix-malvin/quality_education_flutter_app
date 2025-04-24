4ea18173b07dfb63a52173545b285f7f55b5e2fd
wilberttanjiro <wilbert.edric@students.mikroskil.ac.id>
2025-04-25 00:53:21 +0700

    Mengubah durasi darui splash screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -12,7 +12,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 1), () {
+    Future.delayed(Duration(seconds: 6), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => GetStartedPage()),
