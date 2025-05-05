582ed34e05e06ff1193d4c0e6622e673b66b016e
felix-malvin <felix.malvin@students.mikroskil.ac.id>
2025-05-05 23:56:58 +0700

    Membuat kelas untuk course badge agar lebih clean code dan re-usable

---
+++ lib/models/course_model.dart
@@ -0,0 +1,55 @@
+import 'package:quality_education_app/models/badge_model.dart';
+
+class Course {
+  final String image;
+  final String name;
+  final String category;
+  final String categoryBaseColor;
+  final String duration;
+  final int totalLessons;
+  final String tutor;
+  final double price;
+  final String level;
+  final double rating;
+  final int totalRatings;
+  final String language;
+  final String totalStudents;
+  final bool isCertificate;
+  final String description;
+  final List<Badge> badges;
+  final List<LessonSection> lessonSections;
+
+  Course({
+    required this.image,
+    required this.name,
+    required this.category,
+    required this.categoryBaseColor,
+    required this.duration,
+    required this.totalLessons,
+    required this.tutor,
+    required this.price,
+    required this.level,
+    required this.rating,
+    required this.totalRatings,
+    required this.language,
+    required this.totalStudents,
+    required this.isCertificate,
+    required this.description,
+    required this.badges,
+    required this.lessonSections,
+  });
+}
+
+class LessonSection {
+  final String sectionTitle;
+  final String duration;
+  final List<String> lessons;
+
+  LessonSection({
+    required this.sectionTitle,
+    required this.duration,
+    required this.lessons,
+  });
+}
+
+
