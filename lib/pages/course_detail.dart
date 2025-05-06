54e6b28b529d83a92eba5d325c88e476b5c1ce47
JohannesHartanto <johannes.hartanto@students.mikroskil.ac.id>
2025-05-06 22:00:26 +0700

    Menambah bottom section

--- lib/pages/course_detail.dart
+++ lib/pages/course_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quality_education_app/models/course_model.dart';
+import 'package:quality_education_app/pages/course_summary.dart';
 
 class CourseDetailPage extends StatefulWidget {
   final Course course;
@@ -85,7 +86,98 @@ class _CourseDetailPageState extends State<CourseDetailPage>
               ],
             ),
           ),
+          _buildBottomSection(media),
         ],
+      ),
+    );
+  }
+
+  Widget _buildBottomSection(media) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        width: media.width,
+        height: 0.12 * media.height,
+        decoration: BoxDecoration(
+          color: Color(0xFFF9F9F9),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 12,
+              offset: const Offset(0, -6), // Shadow ke atas
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total price',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color(0xFF2E2E2E),
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: 1),
+                    Text(
+                      'Rp 160.000',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xFF0066FF),
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CourseSummaryPage(),
+                        ),
+                      );
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xFF0066FF),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 35,
+                        vertical: 15,
+                      ),
+                    ),
+                    child: Text(
+                      "Enroll Now",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xFFF9F9F9),
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+          ],
+        ),
       ),
     );
   }
