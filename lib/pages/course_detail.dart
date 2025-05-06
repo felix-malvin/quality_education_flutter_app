a9c598ea3f2623a24c5569051c4790b114bc638a
JohannesHartanto <johannes.hartanto@students.mikroskil.ac.id>
2025-05-06 21:54:34 +0700

    Membuat template dasar untuk halaman Course Detail

--- lib/pages/course_detail.dart
+++ lib/pages/course_detail.dart
@@ -1,16 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:quality_education_app/models/course_model.dart';
 
-class CourseDetailPage extends StatelessWidget {
-  const CourseDetailPage({super.key});
+class CourseDetailPage extends StatefulWidget {
+  final Course course;
+
+  const CourseDetailPage({Key? key, required this.course}) : super(key: key);
+
+  @override
+  _CourseDetailPageState createState() => _CourseDetailPageState();
+}
+
+class _CourseDetailPageState extends State<CourseDetailPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int lessonCounter = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Course Detail Page',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color(0xFFF9F9F9),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Image.asset(
+            widget.course.image,
+            width: media.width,
+            height: 0.7 * media.width,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            width: media.width,
+            height: 0.8 * media.width,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF2E2E2E),
+                  Colors.transparent,
+                  Color(0xFF2E2E2E),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25),
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 0.7 * media.width - 60),
+                    Container(
+                      width: media.width,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF9F9F9),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                        ]
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
