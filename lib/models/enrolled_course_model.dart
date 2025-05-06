502fa15507c44307693834a951fa5f2706129c18
JohannesHartanto <johannes.hartanto@students.mikroskil.ac.id>
2025-05-07 00:34:01 +0700

    Menambahkan kelas enrolled course

---
+++ lib/models/enrolled_course_model.dart
@@ -0,0 +1,17 @@
+import 'package:quality_education_app/models/course_model.dart';
+
+class EnrolledCourse {
+  final String id;
+  final DateTime enrolledAt;
+  final String paymentMethod;
+  final int progress;
+  final Course enrolledCourse;
+
+  EnrolledCourse({
+    required this.id,
+    required this.enrolledAt,
+    required this.paymentMethod,
+    required this.progress,
+    required this.enrolledCourse,
+  });
+}
