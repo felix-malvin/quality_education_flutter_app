9423623e80d0276ad01a5a712dc0102eb360a318
erikhmli <erik.hamli@students.mikroskil.ac.id>
2025-06-29 22:31:49 +0700

    Menambahkan widget Divider

--- lib/pages/receipt.dart
+++ lib/pages/receipt.dart
@@ -38,5 +38,84 @@ class _ReceiptPage extends State<ReceiptPage> {
     );
   }
 
-  
+  Widget _buildRowContent(String title, content) {
+    return Column(
+      children: [
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                color: CustomColors.secondaryText,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Text(
+              content,
+              style: TextStyle(
+                color: CustomColors.primaryText,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBottomSection(media) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        width: media.width,
+        height: 0.12 * media.height,
+        decoration: BoxDecoration(
+          color: CustomColors.white,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 12,
+              offset: const Offset(0, -6), // Shadow ke atas
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: CustomColors.primary,
+                  foregroundColor: CustomColors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 35, vertical: 15),
+                ),
+                child: Text(
+                  "Download E-Receipt",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFFF9F9F9),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
 }
