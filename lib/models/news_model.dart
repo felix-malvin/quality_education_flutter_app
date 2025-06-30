2a759938453b147141f1e0b4a353f42ce1d40f1d
afriyoga344 <afriyoga.haganta@students.mikroskil.ac.id>
2025-06-30 23:33:33 +0700

    Menambahan kelas News

---
+++ lib/models/news_model.dart
@@ -0,0 +1,19 @@
+class News {
+  final String id;
+  final String title;
+  final String image;
+  final String author;
+  final String time;
+  final String category;
+  final String tag;
+
+  News({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.author,
+    required this.time,
+    required this.category,
+    required this.tag,
+  });
+}
