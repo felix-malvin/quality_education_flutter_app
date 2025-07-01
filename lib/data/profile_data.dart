5134da12b693f52082152e6c31f3d48a679523a3
JohannesHartanto <johannes.hartanto@students.mikroskil.ac.id>
2025-07-01 23:51:02 +0700

    Menambahkan kelas Profile

---
+++ lib/data/profile_data.dart
@@ -0,0 +1,8 @@
+Map<String, dynamic> profileData = {
+  'name': 'Gojo Satoru',
+  'username': 'satorugojo',
+  'phone': '081234567890',
+  'email': 'satoru_gojo@gmail.com',
+  'gender': 'Male',
+  'birthDate': '2000-01-01',
+};
