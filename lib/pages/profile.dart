5c3db1b3ec52d39e3badb22adc674ef15c18144d
JohannesHartanto <johannes.hartanto@students.mikroskil.ac.id>
2025-07-01 23:24:58 +0700

    Memodifikasi Profile Page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:quality_education_app/widgets/appbar/custom_appbar.dart';
+import 'package:quality_education_app/commons/color.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  _ProfilePage createState() => _ProfilePage();
+}
+
+class _ProfilePage extends State<ProfilePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    var media = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Color(0xFFF9F9F9),
+      appBar: CustomAppBar(title: 'Profile'),
       body: Center(
-        child: Text(
-          'Profile Page',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        child: Column(
+          children: [
+            _buildProfileHeader(),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    _buildAccountSection(),
+                    _buildSectionDivider(media),
+                    _buildHelpSection(),
+                    _buildSectionDivider(media),
+                    _buildAboutSection(),
+                    _buildBottomSection(media),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
