8af66a1b8c6a06ffdae8cd9a87487ada5a56fa9d
wilberttanjiro <wilbert.edric@students.mikroskil.ac.id>
2025-07-02 22:27:02 +0700

    Menambahkan list Notifikasi

--- lib/pages/inbox.dart
+++ lib/pages/inbox.dart
@@ -10,6 +10,51 @@ class InboxPage extends StatefulWidget {
 }
 
 class _InboxPageState extends State<InboxPage> {
+  List<Map<String, dynamic>> rawNotifications = [
+    {
+      'avatar': Icons.school,
+      'title': 'New Course Available!',
+      'content': 'Check out the latest course tailored for you.',
+      'date': '2025-06-27 14:30:00',
+      'isRead': false,
+    },
+    {
+      'avatar': Icons.star,
+      'title': 'Rate Your Course',
+      'content': 'Help us improve by rating your course.',
+      'date': '2025-06-27 10:15:00',
+      'isRead': false,
+    },
+    {
+      'avatar': Icons.notifications,
+      'title': 'System Update',
+      'content': 'The app has been updated to the latest version.',
+      'date': '2025-06-26 09:45:00',
+      'isRead': false,
+    },
+    {
+      'avatar': Icons.local_offer,
+      'title': 'Special Discount!',
+      'content': 'Get 20% off on selected courses this week.',
+      'date': '2025-06-26 16:30:00',
+      'isRead': false,
+    },
+    {
+      'avatar': Icons.lock,
+      'title': 'Password Changed',
+      'content': 'Your password was changed successfully.',
+      'date': '2025-06-25 08:20:00',
+      'isRead': false,
+    },
+    {
+      'avatar': Icons.emoji_events,
+      'title': 'You Earned a Badge!',
+      'content': 'Congrats! You earned the "Fast Learner" badge.',
+      'date': '2025-06-25 14:05:00',
+      'isRead': false,
+    },
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
