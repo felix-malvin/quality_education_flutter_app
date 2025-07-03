8796fa3b42cb4180f37bd27c37273c9deb5b16ca
erikhmli <erik.hamli@students.mikroskil.ac.id>
2025-07-03 23:53:37 +0700

    Menambahkan Components EnrolledCourseCard

---
+++ lib/widgets/components/enrolled_course_card.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quality_education_app/pages/lessons.dart';
+import 'package:quality_education_app/widgets/widget_support/textstyle.dart';
+import 'package:quality_education_app/models/enrolled_course_model.dart';
+
+class EnrolledCourseCard extends StatefulWidget {
+  const EnrolledCourseCard({super.key});
+
+  @override
+  State<EnrolledCourseCard> createState() => _EnrolledCourseCardState();
+}
+
+class _EnrolledCourseCardState extends State<EnrolledCourseCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
