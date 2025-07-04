af7d7a8c5a54a98d00f155b042ef049e932bd2a7
JohannesHartanto <johannes.hartanto@students.mikroskil.ac.id>
2025-07-04 23:27:58 +0700

    Menginisialisasi fungsi pickDate

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -11,13 +11,32 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _gender;
+  DateTime? _birthDate;
+
+  final _nameController = TextEditingController();
+  final _usernameController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _emailController = TextEditingController();
+
+  final _nameFocus = FocusNode();
+  final _usernameFocus = FocusNode();
+  final _phoneFocus = FocusNode();
+  final _emailFocus = FocusNode();
+
+  Future<void> _pickDate() async {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: "Edit Profile"),
       backgroundColor: CustomColors.white,
       body: Center(
-        child: Text(""),
+        child: Text("Edit Profile Page"),
       ),
     );
   }
