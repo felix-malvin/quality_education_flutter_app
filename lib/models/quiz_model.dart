e08fd2b0db34871aa6b09e26bf6a7d7b733c5f42
afriyoga344 <afriyoga.haganta@students.mikroskil.ac.id>
2025-07-06 00:21:03 +0700

    Membangun kelas Quiz

---
+++ lib/models/quiz_model.dart
@@ -0,0 +1,11 @@
+class Quiz {
+  final String question;
+  final List options;
+  final String answer;
+
+  Quiz({
+    required this.question,
+    required this.options,
+    required this.answer,
+  });
+}
